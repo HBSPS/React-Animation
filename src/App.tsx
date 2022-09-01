5566606207b476a0d0ee36e9c2108580e4647344
HBSPS <wiyeong79@naver.com>
2022-09-02 00:49:53 +0900

    :sparkles: Drag
    Drag 및 whileDrag 추가

--- src/App.tsx
+++ src/App.tsx
@@ -19,13 +19,14 @@ const Box = styled(motion.div)`
 
 const boxVariants = {
   hover: { scale: 1.5, rotate: 90 },
-  click: {scale: 1, borderRadius: "100px"}
+  click: { scale: 1, borderRadius: "100px" },
+  drag: { backgroundColor: "rgb(46, 204, 133)", transition: { duration: 10 } }
 };
 
 function App() {
   return (
     <Wrapper>
-      <Box variants={boxVariants} whileHover="hover" whileTap="click" />
+      <Box drag variants={boxVariants} whileHover="hover" whileTap="click" whileDrag="drag" />
     </Wrapper>
   );
 };
