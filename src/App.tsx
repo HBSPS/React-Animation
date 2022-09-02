cacf8db38ce9c8a7228597a85e05ee88dcc84fbd
HBSPS <wiyeong79@naver.com>
2022-09-02 23:49:07 +0900

    :sparkles: Slider
    Slider 만들기

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ const Wrapper = styled(motion.div)`
   display: flex;
   justify-content: center;
   align-items: center;
+  flex-direction: column;
   background: linear-gradient(135deg,rgb(238, 0, 153),rgb(221, 0, 238));
 `;
 
@@ -18,35 +19,47 @@ const Box = styled(motion.div)`
   border-radius: 40px;
   position: absolute;
   top: 100px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const boxVariants = {
-  initial: {
+const box = {
+  invisible: {
+    x: 500,
     opacity: 0,
     scale: 0
   },
   visible: {
+    x: 0,
     opacity: 1,
     scale: 1,
-    rotate: 360
+    transition: {
+      duration: 0.5
+    }
   },
-  leaving: {
+  exit: {
+    x: -500,
     opacity: 0,
     scale: 0,
-    y: 20
+    transition: {
+      duration: 0.5
+    }
   }
-};
+}
 
 function App() {
-  const [showing, setShowing] = useState(true);
-  const toggleShowing = () => setShowing((prev) => !prev);
+  const [visible, setVisible] = useState(1);
+  const next = () => setVisible((prev) => prev === 10 ? 10 : prev + 1);
+  const prev = () => setVisible((prev) => prev === 1 ? 1 : prev - 1);
   return (
     <Wrapper>
-      <button onClick={toggleShowing}>Click Me</button>
       <AnimatePresence>
-        {showing ? <Box variants={boxVariants} initial="initial" animate="visible" exit="leaving"/> : null}
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((i) => i === visible ? <Box key={i} variants={box} initial="invisible" animate="visible" exit="exit">{i}</Box> : null)}
       </AnimatePresence>
+      <button onClick={prev}>Prev</button>
+      <button onClick={next}>Next</button>
     </Wrapper>
   );
 };
