00957d8a8a9d9d52f9435a9d6e33e4fb1d998503
HBSPS <wiyeong79@naver.com>
2022-09-02 21:29:09 +0900

    :sparkles: MotionValue
    useMotionValue를 사용해 값 받아오기 및 console.log로 확인하는 방법

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
-import { motion } from "framer-motion";
-import { useRef } from "react";
+import { motion, useMotionValue } from "framer-motion";
+import { useEffect } from "react";
 
 const Wrapper = styled.div`
   height: 100vh;
@@ -10,16 +10,6 @@ const Wrapper = styled.div`
   align-items: center;
 `;
 
-const BiggerBox = styled.div`
-  width: 300px;
-  height: 300px;
-  background-color: rgba(255, 255, 255, 0.4);
-  border-radius: 40px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  overflow: hidden;
-`;
 
 const Box = styled(motion.div)`
   width: 100px;
@@ -29,18 +19,13 @@ const Box = styled(motion.div)`
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const boxVariants = {
-  hover: { scale: 1.5, rotate: 90 },
-  click: { scale: 1, borderRadius: "100px" },
-};
-
 function App() {
-  const biggerBoxRef = useRef<HTMLDivElement>(null)
+  const x = useMotionValue(0);
+
   return (
     <Wrapper>
-      <BiggerBox ref={biggerBoxRef}>
-        <Box drag dragSnapToOrigin dragConstraints={biggerBoxRef} variants={boxVariants} whileHover="hover" whileTap="click" />
-      </BiggerBox>
+      <button onClick={() => x.set(200)}>Click Me</button>
+      <Box style={{x: x}} drag="x" dragSnapToOrigin />
     </Wrapper>
   );
 };
