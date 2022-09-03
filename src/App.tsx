6ff539a3f4642bb88b0fb925c77a385bd4ad51ed
HBSPS <wiyeong79@naver.com>
2022-09-03 22:37:41 +0900

    :sparkles: Finish!
    렌더링하며 layoutId를 부여하고 연결해해 애니메이션 만들기

--- src/App.tsx
+++ src/App.tsx
@@ -43,22 +43,16 @@ const Overlay = styled(motion.div)`
 `;
 
 function App() {
-  const [clicked, setClicked] = useState(false);
-  const toggle = () => {
-    setClicked((prev) => !prev);
-  };
+  const [Id, setId] = useState<null | string>(null);
   return (
-    <Wrapper onClick={toggle}>
+    <Wrapper>
       <Grid>
-        <Box layoutId="hello"/>
-        <Box />
-        <Box />
-        <Box />
+        {["1", "2", "3", "4"].map((n) => <Box onClick={() => setId(n)} key={n} layoutId={n} />)}
       </Grid>
       <AnimatePresence>
-        {clicked ?
-          <Overlay initial={{ backgroundColor: "rgba(0, 0, 0, 0)" }} animate={{ backgroundColor: "rgba(0, 0, 0, 0.5)" }} exit={{ backgroundColor: "rgba(0, 0, 0, 0)" }}>
-            <Box style={{width: 500, height: 250}} layoutId="hello"/>
+        {Id ?
+          <Overlay onClick={() => setId(null)} initial={{ backgroundColor: "rgba(0, 0, 0, 0)" }} animate={{ backgroundColor: "rgba(0, 0, 0, 0.5)" }} exit={{ backgroundColor: "rgba(0, 0, 0, 0)" }}>
+            <Box style={{ width: 500, height: 250 }} layoutId={Id} />
           </Overlay>
           : null}
       </AnimatePresence>
